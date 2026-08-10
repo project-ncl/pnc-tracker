67cc0bd50b2d1273f78f3405bbe8e6256522bfb8
Petr Kočandrle <pkocandr@ibm.com>
2026-08-10 23:48:26 +0200

    Remove unwanted property

--- src/main/java/org/jboss/pnc/tracker/service/ArtifactoryConnector.java
+++ src/main/java/org/jboss/pnc/tracker/service/ArtifactoryConnector.java
@@ -7,13 +7,9 @@ package org.jboss.pnc.tracker.service;
 import org.jboss.pnc.tracker.model.DbPackageType;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class ArtifactoryConnector {
-
-    @ConfigProperty(name = "artifactory.token")
-    String artifactoryToken;
 
     public ArtifactoryConnector() {
         // TODO Auto-generated constructor stub
